96404439f67c49c73ede392d39693bafd60484ec
Angel Deluna <angelluna2016@gmail.com>
2021-02-19 10:55:41 -0500

    Added api injecting

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    const $trackContainer = $("#chart")
+    
+    $.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json")
+        .then((data) => {
+            chartData = data.tracks.track 
+            console.log(chartData)
+            let chartHTML = renderChart(chartData)
+
+            $trackContainer.html(chartHTML)
+        })
+
+
+
+
+})
+
+function renderChart(chartArray){
+
+    let trackData = ""
+
+    const chartHtmlArray = chartArray.map((currentTrack) => {
+        $.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7781e08c195a7c652f6a3d277ef99b45&artist=${currentTrack.artist.name}&track=${currentTrack.name}&format=json`)
+            .then((data) => {
+                trackData = data.track
+                // console.log( trackData.duration)
+            
+            })
+            console.log(trackData.duration)
+        return `<div>${currentTrack.name}/ ${currentTrack.artist.name}/ ${currentTrack.playcount}/{trackData.album.title} ${trackData.duration} </div>`
+    })
+    return chartHtmlArray.join("")
+}
