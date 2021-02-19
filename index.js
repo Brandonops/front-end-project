8ff930c9bd78db27dc9a11a50b0df6ae1b549494
Angel Deluna <angelluna2016@gmail.com>
2021-02-19 16:39:13 -0500

    added api chart to html

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    const $trackContainer = $("#chart")
+    const $trackContainer = $("#tbody")
 
     $.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json")
         .then((data) => {
@@ -16,15 +16,24 @@ $(document).ready(function () {
 })
 
  async function renderChart(chartArray) {
-
-    const chartPromise = chartArray.map(async(currentTrack) => {
-
+    // let count =1
+    const chartPromise = chartArray.map(async(currentTrack,index) => {
+        
         return await $.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7781e08c195a7c652f6a3d277ef99b45&artist=${currentTrack.artist.name}&track=${currentTrack.name}&format=json`)
             .then( (data) => {
                 trackData = data.track
                 console.log(trackData)
-                
-                return `<div>${currentTrack.name} ${currentTrack.artist.name}/ ${currentTrack.playcount}/${trackData.album? trackData.album.title : ""} ${trackData.duration} </div>`
+                let sec = trackData.duration/1000%60
+                sec = sec < 10? `0${sec}`: sec
+                let min = Math.floor(trackData.duration/1000/60)
+                let time = trackData.duration == 0? "": `${min}:${sec}`
+                return `<tr>
+                <th scope="row">${index+1}</th>
+                <td>${(currentTrack.name).toUpperCase()}</td>
+                <td>${currentTrack.artist.name}</td>
+                <td>${trackData.album? trackData.album.title : "No Album"}</td>
+                <td> ${time}</td>
+              </tr>`
             })
 
     })
