32f78e7a151cb2cf189ebb2b18c169fb3481edba
Angel Deluna <angelluna2016@gmail.com>
2021-02-19 11:57:41 -0500

    added changes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     const $trackContainer = $("#chart")
-    
+
     $.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json")
         .then((data) => {
-            chartData = data.tracks.track 
-            console.log(chartData)
+            chartData = data.tracks.track
+            // console.log(chartData)
             let chartHTML = renderChart(chartData)
-
             $trackContainer.html(chartHTML)
         })
 
@@ -15,18 +14,18 @@ $(document).ready(function () {
 
 })
 
-function renderChart(chartArray){
-
-    let trackData = ""
+ function renderChart(chartArray) {
 
     const chartHtmlArray = chartArray.map((currentTrack) => {
+
         $.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7781e08c195a7c652f6a3d277ef99b45&artist=${currentTrack.artist.name}&track=${currentTrack.name}&format=json`)
             .then((data) => {
                 trackData = data.track
-                console.log( trackData.duration)
-            
+                // console.log(trackData.duration)
+
             })
-        return `<div>${currentTrack.name}/ ${currentTrack.artist.name}/ ${currentTrack.playcount}/{trackData.album.title} ${trackData.duration} </div>`
+     return `<div>${currentTrack.name}/ ${currentTrack.artist.name}/ ${currentTrack.playcount}/{trackData.album.title} {trackData.duration} </div>`
+
     })
     return chartHtmlArray.join("")
 }
