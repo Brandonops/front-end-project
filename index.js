942ded743769d66aae9eba714a14414e27ead65d
Angel Deluna <angelluna2016@gmail.com>
2021-02-22 14:25:14 -0500

    added js to song.html

--- index.js
+++ index.js
@@ -12,9 +12,28 @@ $(document).ready(function () {
             })
         })
 
+    const $searchBar = $("#search-form")
+    $searchBar.on('submit', function (e) {
+
+        e.preventDefault()
+        const $searchBtn = $("#search")
+        const search = $searchBar.val()
+        search = encodeURIComponent(search)
+
+
+    })
+
 
 })
+function addToLocal(song,artist)
+{
+    let track = {song,artist}
 
+    trackJSON = JSON.stringify(track)
+    console.log(trackJSON)
+    localStorage.setItem('song',trackJSON)
+    window.location.href ="./song.html"
+}
  async function renderChart(chartArray) {
     // let count =1
     const chartPromise = chartArray.map(async(currentTrack,index) => {
@@ -22,17 +41,16 @@ $(document).ready(function () {
         return await $.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7781e08c195a7c652f6a3d277ef99b45&artist=${currentTrack.artist.name}&track=${currentTrack.name}&format=json`)
             .then( (data) => {
                 trackData = data.track
-                console.log(trackData)
                 let sec = trackData.duration/1000%60
                 sec = sec < 10? `0${sec}`: sec
                 let min = Math.floor(trackData.duration/1000/60)
                 let time = trackData.duration == 0? "": `${min}:${sec}`
-                return `<tr >
+                return `<tr>
                 <th scope="row">${index+1}</th>
-                <td>${(currentTrack.name).toUpperCase()}</td>
-                <td>${currentTrack.artist.name}</td>
+                <td><a href="#" onclick ="addToLocal('${currentTrack.name}', '${currentTrack.artist.name}')">${(currentTrack.name).toUpperCase()} </a></td>
+                <td> <a href= "#">${currentTrack.artist.name} </a></td>
                 <td>${trackData.album? trackData.album.title : "No Album"}</td>
-                <td> ${time}</td>
+                <td> ${time}</td> 
               </tr>`
             })
 
