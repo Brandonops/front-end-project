7abd710b9f9125a78ada8a7bd7bc31ceb05da8b8
Brandon <woolstonbj11@gmail.com>
2021-02-22 10:34:40 -0500

    added artist.js, implemented api

--- artist.js
+++ artist.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-    const $bioContainer = $(".biocontainer")
-    artistData = data.artist.summary
-
-    let artistHtmlArray = renderChart(artistData)
-
+  const $bioContainer = $(".bioContainer")
+      fetch('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json&format=json')
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+  
+          let $artistBioSummary = data.artist.bio.summary
+          console.log($artistBioSummary);
+          $bioContainer.html($artistBioSummary)
+  
+        })
+        .catch((error) => {
+          console.log(error);
 })
-
+      })
