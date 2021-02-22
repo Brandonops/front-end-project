cce255833c92ce53124c3a85f5c3fb00cc9ebea7
Brandon <woolstonbj11@gmail.com>
2021-02-22 17:37:54 -0500

    reformatted rendering and fetch functions

--- artist.js
+++ artist.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
   const $artistContainer = $("#tbody")
 
   fetch('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json')
-  .then(function (response) { return response.json(); })
-  .then(function (data) {
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
 
-    let $artistChart = data.artists.artist
-    console.log($artistChart);
-    $artistContainer.html(renderChart($artistChart));
-    console.log(data.artists.artist);
-    
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+      let $artistChart = data.artists.artist
+      console.log($artistChart);
+      $artistContainer.html(renderChart($artistChart));
+      console.log(data.artists.artist);
+
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 
 
 })
@@ -23,13 +23,13 @@ function addingCommas(x) {
 }
 
 function renderChart(chartArray) {
-  const $artistChartHtmlArray = chartArray.map(function (currentArtist,index) {
+  const $artistChartHtmlArray = chartArray.map(function (currentArtist, index) {
 
-      let listenersWCommas = addingCommas(`${currentArtist.listeners}`)
-      let playcountWCommas = addingCommas(`${currentArtist.playcount}`)
-      return `<tr>
+    let listenersWCommas = addingCommas(`${currentArtist.listeners}`)
+    let playcountWCommas = addingCommas(`${currentArtist.playcount}`)
+    return `<tr>
 
-      <th scope="row">${index+1}</th>
+      <th scope="row">${index + 1}</th>
       <td>${(currentArtist.name).toUpperCase()}</td>
       <td>${listenersWCommas}</td>
       <td>${playcountWCommas}</td>
@@ -43,33 +43,52 @@ function renderChart(chartArray) {
 
 const $myForm = $("#search-form");
 $myForm.on("submit", (e) => {
-    
-    const searchString = document.querySelector("#search-bar-input").value;
-    const urlEncodedSearchString = encodeURIComponent(searchString);
-    e.preventDefault();
-    console.log(searchString);
-    console.log(urlEncodedSearchString);
 
-fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${urlEncodedSearchString}&api_key=http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${urlEncodedSearchString}&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json&format=json`)
+  const searchString = document.querySelector("#search-bar-input").value;
+  const urlEncodedSearchString = encodeURIComponent(searchString);
+  e.preventDefault();
+  console.log(searchString);
+  console.log(urlEncodedSearchString);
+  renderArtist(urlEncodedSearchString);
+
+  
+});
+function renderArtist(artist) {
+fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json&format=json`)
   .then(function (response) { return response.json(); })
   .then(function (data) {
 
-    let $artistContainer = $("#tbody")
-    let $artistBioSummary = data.artist.bio.content
+    let $artistContainer = $("#table-chart")
+    let $bioContainer = $(".content")
+    let $artistBioSummary = data.artist
+    $("#mainArtistJumbo").css("display","none");
     console.log($artistBioSummary);
 
-    $artistContainer.html(renderBio($artistBioSummary));
+    $artistContainer.html("");
+    $bioContainer.html(renderartistbio($artistBioSummary));
+
 
   })
   .catch((error) => {
     console.log(error);
   })
+}
+function renderartistbio(artist) {
+  return `
+        <div class="jumbotron jumbotron-fluid d-flex ml-10">
+          <img src="test-image.jpeg" style=" width: 200px; height: 200px;">
+        <div class="container">
+          <h1 class="display-4" id="h1-title">${artist.name}</h1>
+          <div id="listeners-and-plays">
+            <p class="lead" id="listeners">Listeners</br>${addingCommas(artist.stats.listeners)}</p>
+            <p class="lead" id="playcount">Plays</br>${addingCommas(artist.stats.playcount)}</p>
+          </div>
+        </div>
+      </div>
 
-});
+      <div id="bioContainer">
+      <h2 style="margin-bottom: 25px";>Artist's Summary:</h2>
+      <p style="line-height: 2em;">${artist.bio.content}</p>
+    `
 
-function renderBio(artistBio) {
-      return `<tr>
-      <td>${(artistBio).toUpperCase()}</td>
-    </tr>`
-
-  }
+}
