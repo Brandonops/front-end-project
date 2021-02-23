b1fc05782ae90e8d263c030c17ddb2db7b1030d6
Angel Deluna <angelluna2016@gmail.com>
2021-02-23 12:49:21 -0500

    added search function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     const $trackContainer = $("#tbody")
-
+    const $jumbo1 = $("#jumbo1")
+    const $jumbo2 = $("#jumbo2")
     $.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json")
         .then((data) => {
             chartData = data.tracks.track
@@ -17,8 +18,19 @@ $(document).ready(function () {
 
         e.preventDefault()
         const $searchBtn = $("#search")
-        const search = $searchBar.val()
+        let search = $searchBtn.val()
         search = encodeURIComponent(search)
+        $.get(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${search}&api_key=7781e08c195a7c652f6a3d277ef99b45&format=json`)
+        .then((data) => {
+            songData = data.results.trackmatches.track
+            console.log(songData)
+            renderSearch(songData)
+            .then((songHTML) => {
+                $jumbo1.html("Top Results")
+                $jumbo2.html("")
+                $trackContainer.html(songHTML)
+            })
+        })
 
 
     })
@@ -44,7 +56,6 @@ function addToLocal(song,artist,tags,date,img)
     window.location.href ="./song.html"
 }
  async function renderChart(chartArray) {
-    // let count =1
     const chartPromise = chartArray.map(async(currentTrack,index) => {
         
         return await $.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7781e08c195a7c652f6a3d277ef99b45&artist=${currentTrack.artist.name}&track=${currentTrack.name}&format=json`)
@@ -68,6 +79,30 @@ function addToLocal(song,artist,tags,date,img)
     return chartHtmlArray.join("")
 }
 
+async function renderSearch(songData){
+    const searchPromise = songData.map(async(currentTrack,index) => {
+        
+        return await $.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7781e08c195a7c652f6a3d277ef99b45&artist=${currentTrack.artist}&track=${currentTrack.name}&format=json`)
+            .then( (data) => {
+                trackData = data.track
+                if (trackData ==undefined)
+                {
+                    return ""
+                }
+                let sec = trackData.duration/1000%60
+                sec = sec < 10? `0${sec}`: sec
+                let min = Math.floor(trackData.duration/1000/60)
+                let time = trackData.duration == 0? "": `${min}:${sec}`
+                return `<tr>
+                <th scope="row">${index+1}</th>
+                <td><a href="#" onclick ="addToLocal('${currentTrack.name}', '${currentTrack.artist}')">${(currentTrack.name).toUpperCase()} </a></td>
+                <td> <a href= "#">${currentTrack.artist} </a></td>
+                <td>${trackData.album? trackData.album.title : "No Album"}</td>
+                <td> ${time}</td> 
+              </tr>`
+            })
 
-
-  
+    })
+   const searchHtmlArray = await Promise.all(searchPromise)
+    return searchHtmlArray.join("")
+}
